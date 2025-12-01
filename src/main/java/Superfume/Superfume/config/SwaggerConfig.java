bce3746b781b987f5908600c8344a061d4b7e599
Ismael Rivas <isma.rivas@duocuc.cl>
2025-12-01 00:16:22 -0300

    Adición Swagger

---
+++ src/main/java/Superfume/Superfume/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package Superfume.Superfume.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Superfume API")
+                        .version("0.0.1")
+                        .description("API pública de Superfume")
+                );
+    }
+
+}
